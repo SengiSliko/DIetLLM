7591da05c15c975c2091184b76aba3be83c2177c
rtsono <sengisliko@icloud.com>
2025-05-26 22:42:30 +0200

    runnable backend

---
+++ Backend/DietLLM/src/test/java/com/dietLLM/DietLLM/DietLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dietLLM.DietLLM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietLlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
